fc4d07acd342e0332b153b82b047bab65d2e6bef
Dmitriy <tchaikovskiy@hotmail.com>
2016-07-15 10:19:13 -0500

    add # noqa to urls to suppress linter warnings
    http://stackoverflow.com/questions/5033727/how-do-i-get-pyflakes-to-ignore-a-statement

--- movie_catalog.py
+++ movie_catalog.py
@@ -11,37 +11,37 @@ toy_story = movie_data_structure.Movie("Toy Story",
 
 avatar = movie_data_structure.Movie("Avatar",
                 "A marine on an alien planet.",
-                "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
                 "https://www.youtube.com/watch?v=uZNHIU3uHT4")
 
 kill_bill = movie_data_structure.Movie("Kill Bill: Volume 1",
                 "A former assassin wakes from a coma and sets off on her quest\
                 for revenge.",
-                "https://upload.wikimedia.org/wikipedia/en/c/cf/Kill_bill_vol_one_ver.jpg",
+                "https://upload.wikimedia.org/wikipedia/en/c/cf/Kill_bill_vol_one_ver.jpg",  # NOQA
                 "https://www.youtube.com/watch?v=ot6C1ZKyiME")
 
 amelie = movie_data_structure.Movie("Amelie",
                     "A young woman who discretely orchestrates the lives of \
                     the people around her, creating a world exclusively of \
                     her own making.",
-                    "https://upload.wikimedia.org/wikipedia/en/5/53/Amelie_poster.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/5/53/Amelie_poster.jpg",  # NOQA
                     "https://www.youtube.com/watch?v=sECzJY07oK4")
 
 youve_got_mail = movie_data_structure.Movie("You've Got Mail",
                     "Two business rivals who hate each other in the office \
                     fall in love over the Internet.",
-                    "https://upload.wikimedia.org/wikipedia/en/e/ee/You%27ve_Got_Mail.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/e/ee/You%27ve_Got_Mail.jpg",  # NOQA
                     "https://www.youtube.com/watch?v=GSeeFgsqUxU")
 
 bourne_identity = movie_data_structure.Movie("The Bourne Identity",
                     "A man is picked up by a fishing boat, bullet-riddled and \
                     suffering from amnesia, before racing to elude assassins \
                     and regain his memory.",
-                    "https://upload.wikimedia.org/wikipedia/en/a/ae/BourneIdentityfilm.jpg",
+                    "https://upload.wikimedia.org/wikipedia/en/a/ae/BourneIdentityfilm.jpg",  # NOQA
                     "https://www.youtube.com/watch?v=cD-uQreIwEk")
 
 # list of movies
-movies = [toy_story, avatar, kill_bill, amelie, 
+movies = [toy_story, avatar, kill_bill, amelie,
           youve_got_mail, bourne_identity]
 
 # generate HTML page
